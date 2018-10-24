6f7f590767dd6dbd70b03d02734452615fbfea21
Simon Holland Flarup <mail@simonflarup.dk>
2018-10-24 22:54:50 +0200

    Fight modifiers (#12)
    * Added strength and accurary modifiers
    
    Includes a couple of bugs that needs to be ironed out. Nameworthy is wrong attack damage.
    
    Also renamed the Characters and Attacks classes to remove the pluralis
    
    * Fixed attack damage not scaling
    
    Attack damages now scale according to the strength of the enemy and the protagonist. Do mind that the values rounded.
    
    Minor bug: Convertion to doubles still leaves the decimal value to 0
    
    * Fixed missing precision
    
    Attacks now correctly typecasts to double, truncating the precision to one decimal. Floored.
    
    * Added accuracy for players and NPC
    
    The hit/miss rate of players have been added based on the accuracy of the spell and the excess castTime.
    For NPCs the rate is dependend on the accuracy of the spell and the reactiontime attribute.
    
    Needs adjusting/balancing.
    
    * Commented out debugging out prints
    
    * Minor syntax updates for CenterText
    
    * Added some javaDoc and victory/defeat message for the fights
    
    * Added Flee option
    
    The Flee option depends on the reactionstimes of both parts. You'll all ways flee from mobs with less reaction than you. You'll have an increasingly smaller chance at fleeing against mobs with higher reactiontime than you. (Mobs with 100 reactiontime more than you will be impossible to flee from)
    
    * Added new TextFormater and child of it
    
    TextFormater replaces old CenterText class that has been set to depricated and should be removed before merge into master. For the fighting system a child of TextFormater, FightTextFormater has been added, that implements a new print() that takes care of the display of health.
    
    The TextFormater should be very versatile.
    
    * Separated classes into their own packages
    
    Development cleanup and organisation
    
    * Added some javaDoc
    
    * Flee chance inverted
    
    I discovered that I inverted the flee chance based on the enemies reactionTime, so that lower reactionTimes (Values above 100) resulted in having a harder time fleeing.

---
+++ src/fightsystem/Attack.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fightsystem;
+
+/**
+ * Important about accuracy
+ * Accuracy is currently implemented in two ways. One for the protagonist, and another for NPCs
+ * For the player the accuracy determines the chance of hitting with a given attack, but you also get a bonus accuracy for speedtyping faster than the castTime (1 second faster equals 10 accuracy)
+ * The above mechanism can be used to remove the timed check, and instead give a accuracy penalty if you use more time than the attacks castTime. (1 second slower equal -10 accuracy)
+ * 
+ * For NPC's the accuracy determines the chance of hitting with a given attack, but it's also depending on the NPC's reaction time.
+ * This means that monsters with lower reaction time (This is better) will have a greater chance of NOT missing an attack.
+ * The reaction time is bound to a Math.random() and simply just adjusts it according to the reactionTime converted to percentage. 
+ * (50 reactionTime and a random value of 0.5 gives an random value of 0.25. This means that for that current turn the attack must have an accuracy above 25 to succeed.)
+ * @author corga and Simon Holland Flarup
+ */
+public class Attack {
+
+    private double dmg;
+    private int castTime;
+    private final String name;
+    private int accuracy;
+ 
+   /**
+    * This constructor is used to create a new attack
+    * @param name the name of the attack. Eg. Hack
+    * @param dmg the damage of the attack (Limited to one decimal precision)
+    * @param accuracy the accuracy of the attack supplied in percentage from 0 to 100.
+    * @param castTime the cast time of the attack, meaning the maximum time it must take to complete the attack (Speedtyping it)
+    */
+    public Attack(String name, double dmg, int accuracy, int castTime) {
+        this.dmg = dmg;
+        this.castTime = castTime;
+        this.name = name;
+        this.accuracy = accuracy;
+    }
+
+    /**
+     * @return the dmg
+     */
+    public double getDmg() {
+        return this.dmg;
+    }
+
+    /**
+     * @return the castTime
+     */
+    public int getCastTime() {
+        return this.castTime;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return this.name;
+    }
+
+    /**
+     * @return the accuracy
+     */
+    public int getAccuracy() {
+        return this.accuracy;
+    }
+
+}
---
+++ src/fightsystem/Character.java
@@ -0,0 +1,113 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fightsystem;
+
+/**
+ *
+ * @author corga
+ */
+public class Character {
+    private double hp, maxHp;
+    private String name;
+    private Attack[] attacks = new Attack[4]; //listed attacks for characters to 4
+    private int reactionTime;
+    private int strength;
+    //private ArrayList<Items> = new ArrayList<Items>(); //Characters has an arraylist containing items.
+
+    /**
+     *
+     */
+    public Character(){
+        this(null, 100, 100, 100);
+    }
+    
+    /**
+     *
+     * @param hp
+     */
+    public Character(double hp){
+        this(null, hp, 100, 100);
+    }
+    /**
+     * This constructor is used to create new monsters
+     * The given hp is automatically set to the mobs maxHp.
+     * If you need to circumsphere this you need to set the HP of the maxHp and call .setHp() with the wished current HP.
+     * @param name the name of the monster. eg. Wombat
+     * @param hp the health of the monster. eg. 100
+     * @param reactionTime the reaction time of the monster. This affects attacks in multiple ways (See attack) The value 100 is default, below is a faster reaction time (percentage) and vice versa.
+     * @param strength the strength of the monster. This affects the damage of attacks. the value 100 is default, below is less damage (percentage) and vice versa
+     */
+    public Character(String name, double hp, int reactionTime, int strength){
+        this.hp = hp;
+        this.maxHp = hp;
+        this.name = name;
+        this.reactionTime = reactionTime;
+        this.strength = strength;
+    }
+
+    /**
+     * @return the hp
+     */
+    public double getHp() {
+        return hp;
+    }
+
+    /**
+     * Sets the HP of the character. If value is higher than the internal maxHp, it defaults to full HP.
+     * @param hp the hp to set
+     */
+    public void setHp(double hp) {
+        if (hp <= this.maxHp) {
+        this.hp = hp;
+        } else {
+            this.hp = this.maxHp;
+        }
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @return the attacks
+     */
+    public Attack[] getAttacks() {
+        return attacks;
+    }
+
+    /**
+     * @param attacks the attacks to set
+     */
+    public void setAttacks(Attack[] attacks) {
+        this.attacks = attacks;
+    }
+
+    /**
+     * @return the reactionTime
+     */
+    public int getReactionTime() {
+        return reactionTime;
+    }
+
+    /**
+     * @return the strength
+     */
+    public int getStrength() {
+        return strength;
+    }
+
+    /**
+     * @param strength the strength to set
+     */
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+    
+    
+}
