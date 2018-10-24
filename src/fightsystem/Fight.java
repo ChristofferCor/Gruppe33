8e912280e15ad6ad705a13ce51b9f3d15b70fae2
Simon Holland Flarup <mail@simonflarup.dk>
2018-10-24 21:41:35 +0200

    Separated classes into their own packages
    Development cleanup and organisation

--- src/fightsystem/Fight.java
+++ src/fightsystem/Fight.java
@@ -1,10 +1,11 @@
-package semestreprojekt.fightsystem;
+package fightsystem;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+import util.FightTextFormater;
 import java.util.Scanner;
 import java.util.Date;
 
--- src/util/TextFormater.java
+++ src/util/TextFormater.java
@@ -1,4 +1,4 @@
-package semestreprojekt.fightsystem;
+package util;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
