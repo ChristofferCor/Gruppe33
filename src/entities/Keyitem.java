7498e25707d2666c7f01e8d93de25868aaafeb45
Sebastian <43139409+semol18@users.noreply.github.com>
2018-10-30 10:36:05 +0100

    Rooms (#7)
    * Rooms added and defined
    
    Added rooms, visit counter and first time events for certain rooms.
    
    Created pseudocode for room items.
    
    * Items and commandos
    
    Added items to rooms and commandos to check inventory, use and take items
    
    * Added REST command
    
    Added the REST command
    
    * Separated the source files from entities (#14)
    
    * Separated the source files from entities
    
    I have separated the source files from the newly added classes. This gives a more structured and clean development environment.
    
    * Moved entities package outside the semestreprojekt package
    
    I did this to match the proposed changes from #12

---
+++ src/entities/Keyitem.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entities;
+
+/**
+ *
+ * @author sebastian
+ */
+public class Keyitem extends Item {
+
+    public Keyitem(String name, int id, String description) {
+        super(name, id, description);
+    }
+}
