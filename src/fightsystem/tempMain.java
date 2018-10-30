a9bbed48af143723753d7d788eed6db407e4ebc1
Christoffer <chrisprosniper@gmail.com>
2018-10-30 13:53:25 +0100

    Fight changes (#19)
    * ArrayList<attack>
    
    Switching ArrayList for attack to be public static insted of private
    
    * Added CONSTANTS in Fight class

--- src/fightsystem/tempMain.java
+++ src/fightsystem/tempMain.java
@@ -6,26 +6,28 @@
 package fightsystem;
 
 import entities.Keyitem;
+
 /**
  *
  * @author corga
  */
 public class tempMain {
-    public static void tempmain(String[] args) {
+
+    public static void main(String[] args) {
         AttackCatalogue browser = new AttackCatalogue();
         Keyitem item = new Keyitem("Nøgle", 1, "Nøglen til livet");
-        
+
         Character protagonist = new Character();
         Attack[] attack = {browser.getAttack("Hack"), browser.getAttack("Slice"), browser.getAttack("Chop")};
         protagonist.setAttacks(attack);
-        //Character wombat = new Character("Wombat", 25, 75, 50);
-        //wombat.setAttacks(attack);
-                protagonist.removeFromInventory(item);
-        System.out.println(protagonist.getInventory().toString());
-        protagonist.addToInventory(item);
-        System.out.println(protagonist.getInventory().toString());
+        Character wombat = new Character("Wombat", 25, 75, 50);
+        wombat.setAttacks(attack);
+        //protagonist.removeFromInventory(item);
+        //System.out.println(protagonist.getInventory().toString());
+        //protagonist.addToInventory(item);
+        //System.out.println(protagonist.getInventory().toString());
 
-        //Fight fight = new Fight(protagonist, wombat);
-        //fight.fight();
+        Fight fight = new Fight(protagonist, wombat);
+        fight.fight();
     }
 }
