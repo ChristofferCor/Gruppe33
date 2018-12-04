e16e8cbd17481d15eb77abddbefc3ceae4bb342b
Christoffer Jensen <chrisprosniper@gmail.com>
2018-12-04 12:25:14 +0100

    javaFX dokumenter og Controller

---
+++ src/gui/Cave6Controller.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.GridPane;
+
+/**
+ * FXML Controller class
+ *
+ * @author corga
+ */
+public class Cave6Controller implements Initializable {
+    @FXML
+    private ImageView player;
+    @FXML
+    private GridPane controlGridPane;
+    @FXML
+    private TextArea outputText;
+    @FXML
+    private Button buttonHelp;
+    @FXML
+    private Button buttonInventory;
+    @FXML
+    private Button buttonRest;
+    @FXML
+    private Button buttonCraft;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void captureKeys(KeyEvent event) {
+    }
+
+    @FXML
+    private void startHelp(ActionEvent event) {
+    }
+
+    @FXML
+    private void startInventory(ActionEvent event) {
+    }
+
+    @FXML
+    private void startRest(ActionEvent event) {
+    }
+
+    @FXML
+    private void startCraft(ActionEvent event) {
+    }
+    
+}
